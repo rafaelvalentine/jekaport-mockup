4da3097830ab7a4c811efda0873a3e6bf6e4a717
lawrenceagles <lawrenceagles@gmail.com>
2019-08-26 19:15:25 +0100

    updated to use css in js style

--- src/pages/RegVehicle.jsx
+++ src/pages/RegVehicle.jsx
@@ -11,46 +11,49 @@ import bgImage from '../images/regVehicle.svg';
 import Title from '../components/Title';
 
 
-const titleStyle = {
-	fontFamily: "Roboto",
-	fontStyle: "normal",
-	fontWeight: "bold",
-	fontSize: "36px",
-	lineHeighteight: "42px",
-	color: "#006494",
-	textAlign: "center",
-	marginTop: "50px"
+
+const styles = {
+	titleStyle:{
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "bold",
+		fontSize: "36px",
+		lineHeighteight: "42px",
+		color: "#006494",
+		textAlign: "center",
+		marginTop: "50px"
+
+	},
+
+	subTitleStyle:{
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "normal",
+		fontSize: "24px",
+		lineHeight: "28px",
+		color: "#65617D",
+		textAlign: "center"
+	},
+
+	RegFormBorder:{
+		background: "#FFFFFF",
+		boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
+		borderRadius: "15px"
+	}
 
 }
 
-const subTitleStyle = {
-	fontFamily: "Roboto",
-	fontStyle: "normal",
-	fontWeight: "normal",
-	fontSize: "24px",
-	lineHeight: "28px",
-	color: "#65617D",
-	textAlign: "center"
-}
-
-
-
-const RegFormBorder  = {
-	background: "#FFFFFF",
-	boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
-	borderRadius: "15px"
-}
 
 const RegVehicle = () => {
 	return (
 		<>
 			<Title 
 				title="Register Your Vehicle" 
-				titleStyle={titleStyle}
 				classes="text-center"
 				subClasses="text-center"
 				subTitle="Signup today and enjoy unlimited patronage" 
-				subTitleStyle={subTitleStyle}
+				titleStyle={styles.titleStyle}
+				subTitleStyle={styles.subTitleStyle}
 			/>
 
 			<Authentication>
@@ -58,7 +61,7 @@ const RegVehicle = () => {
 					<img src={bgImage} alt="Registration page background image" />
 				</div>
 
-				<div style={RegFormBorder} className="col-md-6 p-5">
+				<div style={styles.RegFormBorder} className="col-md-6 p-5">
 					<RegVehicleForm />
 				</div>
 			</Authentication>
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -5,68 +5,66 @@ import Title from '../components/Title';
 import Password from '../components/Passwords';
 
 
-const titleStyle = {
-	fontFamily: "Roboto",
-	fontStyle: "normal",
-	fontWeight: "bold",
-	fontSize: "36px",
-	lineHeighteight: "42px",
-	color: "#006494",
-	marginTop: "20px",
-	marginBottom: "23px"
+const styles = {
+	titleStyle:{
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "bold",
+		fontSize: "36px",
+		lineHeighteight: "42px",
+		color: "#006494",
+		marginTop: "20px",
+		marginBottom: "23px"
+	},
+	formStyle:{
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "bold",
+		fontWeight: "bold",
+		fontSize: "18px",
+		lineHeight: "21px"
+	},
+	card:{
+		boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
+		borderRadius: "15px"
+	},
+	btnStyle:{
+		background: "#006494",
+		backdropFilter: "blur(4px)",
+		borderRadius: "5px",
+		borderColor: "#006494",
+		color: "#FFF"
+	}
 
-}
-
-
-const formStyle = {
-	fontFamily: "Roboto",
-	fontStyle: "normal",
-	fontWeight: "bold",
-	fontWeight: "bold",
-	fontSize: "18px",
-	lineHeight: "21px"
-}
-
-const btnStyle = {
-	background: "#006494",
-	backdropFilter: "blur(4px)",
-	borderRadius: "5px",
-	borderColor: "#006494",
-	color: "#FFF"
-}
-
-const card = {
-	boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
-	borderRadius: "15px"
 }
 
 const ResetPassword = () => {
 	return (
 		<Password>
 			<div className="col-sm-6 mx-auto">
-				<div className="card justify-content-center p-5" style={card}>
+				<div className="card justify-content-center p-5" style={styles.card}>
 					<div className="card-body">
 
 						<Title 
 							title="Reset Password" 
 							classes="text-center"
-							titleStyle={titleStyle}
+							titleStyle={styles.titleStyle}
 						/>
 
 
 						<form>
 							<div className="form-group py-2">
-								<label style={formStyle} htmlFor="password">New Password</label>
+								<label style={styles.formStyle} htmlFor="password">New Password</label>
 								<input type="password" className="form-control" id="password" placeholder="Enter Password" />
 							</div>
 
 							<div className="form-group py2">
-								<label style={formStyle} htmlFor="confirmPassword">Confirm Password</label>
+								<label style={styles.formStyle} htmlFor="confirmPassword">Confirm Password</label>
 								<input type="password" className="form-control" id="confirmPassword" placeholder="Enter Password" />
 							</div>
 
 						  <div className="d-flex justify-content-center align-items-center mt-5">
-						  	<button style={btnStyle} className="btn btn-lg btn-block btn-custom">Submit</button>
+						  	<button style={styles.btnStyle} className="btn btn-lg btn-block btn-custom">Submit</button>
 						  </div>
 
 						</form>
