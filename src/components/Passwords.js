31ab9f8963767adef513ed97737186697176e4a2
lawrenceagles <lawrenceagles@gmail.com>
2019-08-26 14:42:47 +0100

    updated styles

--- src/components/Passwords.js
+++ src/components/Passwords.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const card = {
+	width: "662px",
+	height: "451px",
 	boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
 	borderRadius: "15px"
 }
@@ -12,8 +14,8 @@ const Passwords = ({children}) => {
 		<>
 			<section className="container">
 				<div className="row vh-100 align-items-center">
-					<div className="col-sm-6 mx-auto">
-					    <div className="card justify-content-center" style={card} >
+					<div className="mx-auto">
+					    <div className="card justify-content-center p-5" style={card} >
 					      <div className="card-body">
 					        {children}
 					      </div>
