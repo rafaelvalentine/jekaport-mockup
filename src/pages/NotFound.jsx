070c77b2ae1eccfd3c36e898321f1091c32b522a
lawrenceagles <lawrenceagles@gmail.com>
2019-08-27 13:16:50 +0100

    cleanup

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -12,8 +12,8 @@ const NotFound = () => {
             <div style={{width: '100%', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                 <Title 
                     title="404" 
-                    titleStyle={{padding: 10}} 
                     subTitle="Oops! The page you are looking for doesn't exist" 
+                    titleStyle={{padding: 10}} 
                     subTitleStyle={{padding: 15, borderLeft: "1px solid #c0c0c0", color: "#c0c0c0"}} 
                 />
             </div>
