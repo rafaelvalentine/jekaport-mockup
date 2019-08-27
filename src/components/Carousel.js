efada952be85f3ef47af81559cb1d9e8f76aaead
lawrenceagles <lawrenceagles@gmail.com>
2019-08-27 20:14:09 +0100

    finished carousel--ads next

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,40 +1,91 @@
 import React from 'react';
 
+// images
+import LionImage from '../images/LionImage.svg';
+
+
+const carouselStyle = {
+	title: {
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "bold",
+		fontSize: "14px",
+		lineHeight: "16px",
+		color: "#006494"
+	},
+	box: {
+		background: "#FFFFFF",
+		boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)",
+		borderRadius: "15px"
+	},
+	boxText: {
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "bold",
+		fontSize: "20px",
+		lineHeight: "23px",
+		color: "rgba(0, 0, 0, 0.7)"
+	},
+	boxSubText: {
+		fontFamily: "Roboto",
+		fontStyle: "normal",
+		fontWeight: "bold",
+		fontSize: "13px",
+		lineHeight: "15px",
+		color: "rgba(0, 0, 0, 0.7)"
+	}
+}
+
 
 const Carousel = ({title}) => {
 	return (
 		<div className="px-5">
-			<h5>{title}</h5>
+			<h5 style={carouselStyle.title}>{title}</h5>
 
-			<article className="d-flex justify-content-between">
+			<article className="d-flex">
 				<span className="align-self-center">&lt;</span>
 				<div class="row px-4">
 					<div class="col-sm-4">
-						<div class="card">
+						<div class="card" style={carouselStyle.box}>
 							<div class="card-body">
-								<h5 class="card-title">Special title treatment</h5>
-								<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-								<a href="#" class="btn btn-primary">Go somewhere</a>
+								<article className="d-flex flex-column flex-md-row">
+									<img src={LionImage} alt="Lions Transport Logo" />
+									<div>
+										<h5 style={carouselStyle.boxText}>Lions Transport</h5>
+										<p style={carouselStyle.boxSubText}>29 Adex Street, Lagos</p>
+										<p style={carouselStyle.boxSubText}>## 234903426435, 2349012323232</p>
+									</div>
+								</article>
 							</div>
 						</div>
 					</div>
 
 				  	<div class="col-sm-4">
-						<div class="card">
+						<div class="card" style={carouselStyle.box}>
 							<div class="card-body">
-								<h5 class="card-title">Special title treatment</h5>
-								<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-								<a href="#" class="btn btn-primary">Go somewhere</a>
+								<article className="d-flex flex-column flex-md-row">
+									<img src={LionImage} alt="Lions Transport Logo" />
+									<div>
+										<h5 style={carouselStyle.boxText}>Lions Transport</h5>
+										<p style={carouselStyle.boxSubText}>29 Adex Street, Lagos</p>
+										<p style={carouselStyle.boxSubText}>## 234903426435, 2349012323232</p>
+									</div>
+								</article>
 							</div>
 						</div>
 					</div>
 
 					<div class="col-sm-4">
-						<div class="card">
+						<div class="card" style={carouselStyle.box}>
 							<div class="card-body">
-								<h5 class="card-title">Special title treatment</h5>
-								<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-								<a href="#" class="btn btn-primary">Go somewhere</a>
+								<article className="d-flex flex-column flex-md-row">
+									<img src={LionImage} alt="Lions Transport Logo" />
+									<div>
+										<h5 style={carouselStyle.boxText}>Lions Transport</h5>
+										<p style={carouselStyle.boxSubText}>29 Adex Street, Lagos</p>
+										<p style={carouselStyle.boxSubText}>## 234903426435, 2349012323232</p>
+									</div>
+								</article>
 							</div>
 						</div>
 					</div>
