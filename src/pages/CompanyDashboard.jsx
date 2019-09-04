4d974b99bc10f24f0dafd37fd0e2dbf14233af56
lawrenceagles <lawrenceagles@gmail.com>
2019-09-04 16:04:33 +0100

    dashboard 90%

--- src/pages/CompanyDashboard.jsx
+++ src/pages/CompanyDashboard.jsx
@@ -8,8 +8,8 @@ const CompanyDashboard = () => {
 		<>
 			<section className="container-fluid">
 				<div className="row">
-					<article className="col-sm-12">
-						<div className="d-flex">
+					<article className="col-sm-12 p-md-3 p-sm-2">
+						<div className="d-flex flex-sm-column flex-md-row">
 							<img src="#" />
 							<p className="mx-md-4"><strong>Dashbaord &#124; Book</strong></p>
 
@@ -18,6 +18,111 @@ const CompanyDashboard = () => {
 							</div>
 						</div>
 					</article>
+
+					<article className="col-sm-12 px-md-5 px-sm-2 py-md-4 py-sm-2">
+						<div className="row">
+							<div className="col-md-4 col-sm-12">
+								<div className="card">
+							      <div className="card-body">
+
+							      	<section className="d-flex flex-md-row flex-column justify-content-around text-center">
+							      		<div>
+							      			<p>TODAY</p>
+							      			<img src="#" />
+							      		</div>
+
+							      		<div>
+							      			<p>560</p>
+							      			<p>Vehicle Listed</p>
+							      		</div>
+							      	</section>
+							        
+							      </div>
+							    </div>
+							</div>
+
+
+							<div className="col-md-4 col-sm-12">
+								<div className="card">
+							      <div className="card-body">
+
+							      	<section className="d-flex flex-md-row flex-column justify-content-around text-center">
+							      		<div>
+							      			<p>TODAY</p>
+							      			<img src="#" />
+							      		</div>
+
+							      		<div>
+							      			<p>560</p>
+							      			<p>Vehicle Listed</p>
+							      		</div>
+							      	</section>
+							        
+							      </div>
+							    </div>
+							</div>
+
+
+							<div className="col-md-4 col-sm-12">
+								<div className="card">
+							      <div className="card-body">
+
+							      	<section className="d-flex flex-md-row flex-column justify-content-around text-center">
+							      		<div>
+							      			<p>TODAY</p>
+							      			<img src="#" />
+							      		</div>
+
+							      		<div>
+							      			<p>560</p>
+							      			<p>Vehicle Listed</p>
+							      		</div>
+							      	</section>
+							        
+							      </div>
+							    </div>
+							</div>
+
+						</div>
+					</article>
+
+					<article className="col-sm-12 p-md-3 p-sm-2">
+						<div className="row">
+							<div className="col-md-7 col-sm-12">
+								<p><strong>GRAPH</strong></p>
+							</div>
+
+							<div className="col-md-5 col-sm-12">
+								<div className="card">
+									<div className="card-body">
+
+										<section className="d-flex flex-column justify-content-around text-center">
+											<p className="text-center">Custom Feedback</p>
+											<p className="text-center">2,800</p>
+
+											<div class="progress m-md-3">
+											  <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+											</div>
+
+											<div className="d-flex flex-column flex-md-row justify-content-around mt-md-3">
+												<div>
+													<p>90%</p>
+													<p>Positive</p>
+												</div>
+
+												<div>
+													<p>10%</p>
+													<p>Negative</p>
+												</div>
+											</div>
+											
+										</section>
+									</div>
+							    </div>
+							</div>
+						</div>
+					</article>
+
 				</div>
 			</section>
 		</>
