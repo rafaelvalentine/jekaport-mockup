bb54c4700660e00073a032c4633cbb3a8d9ca381
lawrenceagles <lawrenceagles@gmail.com>
2019-09-05 18:54:28 +0100

    modal

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Modal from 'react-awesome-modal';
+import { Link } from 'react-router-dom';
 
 // components
 import Title from '../components/Title';
@@ -37,41 +39,72 @@ const styles = {
 
 }
 
-const ResetPassword = () => {
-	return (
-		<Password>
-			<div className="col-sm-6 mx-auto">
-				<div className="card justify-content-center p-5" style={styles.card}>
-					<div className="card-body">
+class ResetPassword extends Component {
 
-						<Title 
-							title="Reset Password" 
-							classes="text-center"
-							titleStyle={styles.titleStyle}
-						/>
+	state = {
+		visible: false
+	}
+
+	openModal = () => {
+		// receive network response
+
+		// update state
+        this.setState({
+            visible : true
+        });
+    }
+
+    closeModal = () => {
+        this.setState({
+            visible : false
+        });
+    }
+
+	render() {
+
+		return (
+			<Password>
+				<div className="col-sm-6 mx-auto">
+					<div className="card justify-content-center p-5" style={styles.card}>
+						<div className="card-body">
+
+							<Title 
+								title="Reset Password" 
+								classes="text-center"
+								titleStyle={styles.titleStyle}
+							/>
 
 
-						<form>
-							<div className="form-group py-2">
-								<label style={styles.formStyle} htmlFor="password">New Password</label>
-								<input type="password" className="form-control" id="password" placeholder="Enter Password" />
-							</div>
+							<form>
+								<div className="form-group py-2">
+									<label style={styles.formStyle} htmlFor="password">New Password</label>
+									<input type="password" className="form-control" id="password" placeholder="Enter Password" />
+								</div>
 
-							<div className="form-group py2">
-								<label style={styles.formStyle} htmlFor="confirmPassword">Confirm Password</label>
-								<input type="password" className="form-control" id="confirmPassword" placeholder="Enter Password" />
-							</div>
+								<div className="form-group py2">
+									<label style={styles.formStyle} htmlFor="confirmPassword">Confirm Password</label>
+									<input type="password" className="form-control" id="confirmPassword" placeholder="Enter Password" />
+								</div>
 
-						  <div className="d-flex justify-content-center align-items-center mt-5">
-						  	<button style={styles.btnStyle} className="btn btn-lg btn-block btn-custom">Submit</button>
-						  </div>
+				                <Modal visible={this.state.visible} width="900" height="300" effect="fadeInUp" onClickAway={() => this.closeModal()}>
+				                    <div className="h-100 d-flex justify-content-center align-items-center flex-column">
+                    					<h2 className="">Password Reset Successful!</h2>
+                    					<p>Your password has been successfully changed</p>             
+				                        <Link to="/login" onClick={() => this.closeModal()}>Click here to return to login</Link>
+					                </div>
+				                </Modal>
 
-						</form>
-					</div>
-			    </div>
-			</div>
-		</Password>
-	);
+							  <div className="d-flex justify-content-center align-items-center mt-5">
+							  	<button type="button" onClick={() => this.openModal()} style={styles.btnStyle} className="btn btn-lg btn-block btn-custom">Submit</button>
+							  </div>
+
+							</form>
+						</div>
+				    </div>
+				</div>
+			</Password>
+		);
+	}
 }
 
 
