2071440235b118b54a54648976935d8f4f0435c8
lawrenceagles <lawrenceagles@gmail.com>
2019-09-12 21:24:09 +0100

    badge 100% icons left

--- src/pages/BusinessDetailDashboard.jsx
+++ src/pages/BusinessDetailDashboard.jsx
@@ -23,6 +23,30 @@ const styles = {
 		fontSize: "20px",
 		lineHeight: "23px",
 		color: "#006494"
+	},
+	badgeDetailsTitle:{
+		fontWeight: "bold",
+		fontSize: "24px",
+		lineHeight: "28px",
+		color: "#000000"
+	},
+	badgeDetailsText: {
+		fontWeight: "bold",
+		fontSize: "18px",
+		lineHeight: "21px",
+		color: "rgba(0, 0, 0, 0.4)"
+	},
+	badgeDetailReview: {
+		fontWeight: "bold",
+		fontSize: "48px",
+		lineHeight: "56px",
+		color: "#006494"
+	},
+	badgeDetailBox: {
+		// border: "1px solid rgba(0, 0, 0, 0.5)",
+		boxSizing: "border-box",
+		boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+		borderRadius: "15px"
 	}
 }
 
@@ -39,19 +63,37 @@ const BusinessDetailDashboard = () => {
 							<p style={styles.badgeAdTitle}>Milo Travels Inc.</p>
 						</div>
 
-						<div id="badge"  className="col-md-9 col-sm-12 d-flex justify-content-between align-items-center p-md-5" style={styles.badge}>
-							<article>
-								<p>Milanovita Travels Inc.</p>
-								<p>item</p>
-								<p>Item</p>
-								<p>Item</p>
-								<p>Item</p>
-								<p>Item</p>
-							</article>
+						<div id="badge"  className="col-md-9 col-sm-12 px-md-5 py-md-3" style={styles.badge}>
+							<div className="row d-flex flex-column flex-md-row justify-content-between align-items-center">
+								<article className="">
+									<p className="p-md-2" style={styles.badgeDetailsTitle}>Milanovita Travels Inc.</p>
+									<p>Bus & Logistics Company</p>
+									<p>Km. 25, Lorem way, Lokoja, Ngeria</p>
+									<p>09000000000, 08000000000</p>
+									<p style={styles.badgeDetailReview}>4.5<span>***</span></p>
+									<p>87 Reviews</p>
+								</article>
 
-							<article>
-								Hello box
-							</article>
+								<article className="w-50">
+									<div class="card" style={styles.badgeDetailBox}>
+										<div class="card-body">
+											<div className="row d-flex justify-content-between p-md-4 p-sm-2">
+												<div><button className="btn btn-small btn-primary btn-block">Book</button></div>
+												<div className="">
+													<p><span>icon</span> Share Url</p>
+													<div>
+														<span>Twitter</span>
+														<span>Instagram</span>
+														<span>Gmail</span>
+														<span>Whatsapp</span>
+													</div>
+													
+												</div>
+											</div>
+										</div>
+									</div>
+								</article>
+							</div>	
 						</div>
 					</article>
 					
