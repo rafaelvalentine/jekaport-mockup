e2e30828a8288eda7d7f46f1b8534e2912a67124
rafaelvalentine <ozichukwuezike@hotmail.com>
2019-10-30 20:25:46 +0100

    added build file

---
+++ build/precache-manifest.0c56e12b026040583c5de735762c9125.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f60948d86131ac698f7a98f1f8a4a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7977afde8a453f90ce5",
+    "url": "/static/css/2.8759d636.chunk.css"
+  },
+  {
+    "revision": "04af5f0aece3b8350a05",
+    "url": "/static/css/main.ac86352c.chunk.css"
+  },
+  {
+    "revision": "d7977afde8a453f90ce5",
+    "url": "/static/js/2.8edd3dcc.chunk.js"
+  },
+  {
+    "revision": "04af5f0aece3b8350a05",
+    "url": "/static/js/main.a0ab4648.chunk.js"
+  },
+  {
+    "revision": "219707186523ad4b8cde",
+    "url": "/static/js/runtime~main.821e3928.js"
+  },
+  {
+    "revision": "5c817722b97796d859e4ce30aa361c14",
+    "url": "/static/media/AvailableCarSeat.5c817722.svg"
+  },
+  {
+    "revision": "a4988c0255077c07526ef22352a5d1d7",
+    "url": "/static/media/BlockedCarSeat.a4988c02.svg"
+  },
+  {
+    "revision": "8e458211306afd81fb6bf8072e261b38",
+    "url": "/static/media/BookVehicleImage.8e458211.svg"
+  },
+  {
+    "revision": "50555ada1878195ae5bf0622e254349a",
+    "url": "/static/media/BookedCarSeat.50555ada.svg"
+  },
+  {
+    "revision": "d75642be0d8216ae76e171eb810fd557",
+    "url": "/static/media/Ellipse.d75642be.svg"
+  },
+  {
+    "revision": "09ed1f1eb8cdf78a320268d6b7711e87",
+    "url": "/static/media/Home.09ed1f1e.svg"
+  },
+  {
+    "revision": "c29e79954608db9b5aa5ccc1b9e0365f",
+    "url": "/static/media/JEKAPORTLOGO_TRANSPARENT 2.c29e7995.svg"
+  },
+  {
+    "revision": "0f57fdf090fb25f23b83712fef13677a",
+    "url": "/static/media/LionImage.0f57fdf0.svg"
+  },
+  {
+    "revision": "ae1eaed5933fa9291e9a9027c81dbf23",
+    "url": "/static/media/SearchResultImage.ae1eaed5.svg"
+  },
+  {
+    "revision": "d0ea87eaf9bbb8343e63efd63310c254",
+    "url": "/static/media/arrow-drop-left.d0ea87ea.svg"
+  },
+  {
+    "revision": "758ebe3ef18d8e8ef9782fc2ce8e80f8",
+    "url": "/static/media/arrow-drop-right.758ebe3e.svg"
+  },
+  {
+    "revision": "11f50c7e23e7b11faee87583ade4cebf",
+    "url": "/static/media/attachment_file.11f50c7e.svg"
+  },
+  {
+    "revision": "0dc46ae93e4d4b350f02b79480255a4c",
+    "url": "/static/media/ball.0dc46ae9.png"
+  },
+  {
+    "revision": "9bfbe9323df60e5c33eff50545be158a",
+    "url": "/static/media/become-a-parnter.9bfbe932.png"
+  },
+  {
+    "revision": "7f15c2d06163881113da5c98901adcd8",
+    "url": "/static/media/bus-active.7f15c2d0.svg"
+  },
+  {
+    "revision": "36a0115cea33b6a30e19efb5074f3e5d",
+    "url": "/static/media/bus.36a0115c.svg"
+  },
+  {
+    "revision": "9b949107aa9c59c1db9fbf7b9bd4e11c",
+    "url": "/static/media/bus.9b949107.svg"
+  },
+  {
+    "revision": "f3e136c518187f8e503075168e836c77",
+    "url": "/static/media/checked.f3e136c5.svg"
+  },
+  {
+    "revision": "270a109fec293906cf9640b35e338408",
+    "url": "/static/media/close-line (3).270a109f.svg"
+  },
+  {
+    "revision": "ae26dcf8f5a053c8110d972f52130b67",
+    "url": "/static/media/close-line-alt.ae26dcf8.svg"
+  },
+  {
+    "revision": "447713fb2f8624620fe463b602ce365b",
+    "url": "/static/media/close-line.447713fb.svg"
+  },
+  {
+    "revision": "98a00f3f6ec591f226955d5b829a9222",
+    "url": "/static/media/dashboard-active.98a00f3f.svg"
+  },
+  {
+    "revision": "8e62bd780a11583b703e0567e0a3001d",
+    "url": "/static/media/dashboard.8e62bd78.svg"
+  },
+  {
+    "revision": "811d921e7c62665fd2cc75e0c30f87f2",
+    "url": "/static/media/default-car.811d921e.svg"
+  },
+  {
+    "revision": "eaecc35205a75ad6bc8a14bea80ea502",
+    "url": "/static/media/down-arrow.eaecc352.svg"
+  },
+  {
+    "revision": "ead161469a2ae99e0242c0e367eb2649",
+    "url": "/static/media/empty-star.ead16146.svg"
+  },
+  {
+    "revision": "64b6d940d0cb1439ce5f1df98a4eb52f",
+    "url": "/static/media/facebook.64b6d940.svg"
+  },
+  {
+    "revision": "cef62595277faa08b88885cf84d7ed5d",
+    "url": "/static/media/fitness.cef62595.png"
+  },
+  {
+    "revision": "a84078b9bf05311fb60a198f2ce7d2fc",
+    "url": "/static/media/forgot.a84078b9.png"
+  },
+  {
+    "revision": "40afd1ed3736bddd2fde8e396c95fdee",
+    "url": "/static/media/home.40afd1ed.svg"
+  },
+  {
+    "revision": "b001b5aa91c644965ad4eb71637c914a",
+    "url": "/static/media/iconfinder_menu-alt_134216.b001b5aa.svg"
+  },
+  {
+    "revision": "ee3067436981a5404c2ef62503b602ad",
+    "url": "/static/media/instagram.ee306743.svg"
+  },
+  {
+    "revision": "4dfaf47bbadc5fa295602cc8f3a2cafb",
+    "url": "/static/media/location.4dfaf47b.svg"
+  },
+  {
+    "revision": "9fb58a13662603604de5b3682a865cdc",
+    "url": "/static/media/login.9fb58a13.png"
+  },
+  {
+    "revision": "09caf51262c325c730c5a7d3305f18c8",
+    "url": "/static/media/logo_for_jekaport.09caf512.svg"
+  },
+  {
+    "revision": "b8131762689161ff2a8e89b8761da867",
+    "url": "/static/media/logout.b8131762.svg"
+  },
+  {
+    "revision": "7a1a5f4c01aaf061d270a7aeb1f9028b",
+    "url": "/static/media/phonenumbers.7a1a5f4c.svg"
+  },
+  {
+    "revision": "9f9ad0dfcdac9a3bde395f66fdffec76",
+    "url": "/static/media/profile-active.9f9ad0df.svg"
+  },
+  {
+    "revision": "840dafa8e106f98a306e9a948d343b97",
+    "url": "/static/media/profile.840dafa8.svg"
+  },
+  {
+    "revision": "8b8c3a71c788d658cf745caa64ef91ac",
+    "url": "/static/media/registration.8b8c3a71.svg"
+  },
+  {
+    "revision": "4e9f38661ebaadcced878b68ecff9885",
+    "url": "/static/media/roses.4e9f3866.png"
+  },
+  {
+    "revision": "4b7fbf0b3d92a43044ef2396ee73432d",
+    "url": "/static/media/settings-active.4b7fbf0b.svg"
+  },
+  {
+    "revision": "e014f4d5bebe8ee531f785d3086fcdbe",
+    "url": "/static/media/settings.e014f4d5.svg"
+  },
+  {
+    "revision": "c733d5622eaf64db677b61f41868f11e",
+    "url": "/static/media/share-option.c733d562.svg"
+  },
+  {
+    "revision": "ead161469a2ae99e0242c0e367eb2649",
+    "url": "/static/media/star.ead16146.svg"
+  },
+  {
+    "revision": "64a5c274de1575f0e6aef68366c9d60b",
+    "url": "/static/media/suitcase-active.64a5c274.svg"
+  },
+  {
+    "revision": "05d00a78d67cbaaee5af909b37127708",
+    "url": "/static/media/suitcase.05d00a78.svg"
+  },
+  {
+    "revision": "1724c4b90974ac6f9a58c72fa23edf28",
+    "url": "/static/media/tag.1724c4b9.svg"
+  },
+  {
+    "revision": "7ae41425c30bac40b17c1683b38a74e8",
+    "url": "/static/media/twitter.7ae41425.svg"
+  }
+]);
